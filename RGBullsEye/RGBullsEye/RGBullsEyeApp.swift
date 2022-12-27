ef8454b13fbe1497eef61396014951476794d41b
Boris <banofon@gmail.com>
2022-12-26 18:22:17 -0600

    First Commit

---
+++ RGBullsEye/RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Boris Ofon on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
